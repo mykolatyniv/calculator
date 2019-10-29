d0abccdb86b463ff1e7cc391f4b0653b0bfae60f
mykolatyniv <ml546@njit.edu>
2019-10-28 22:24:39 -0400

    Fix: fixed calc files

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,35 +1,25 @@
 def addition(a, b):
-    a = int(a)
-    b = int(b)
     c = a + b
     return c
 
 def subtraction(a, b):
-    a = int(a)
-    b = int(b)
-    c = a - b
+    c = b - a
     return c
 
 def multiplication(a, b):
-    a = int(a)
-    b = int(b)
     c = a * b
     return c
 
 def division(a, b):
-    a = int(a)
-    b = int(b)
-    c = a / b
+    c = b / a
     return c
 
 def square(a):
-    a = int(a)
-    c = a**2
+    c = a ** 2
     return c
 
 def square_root(a):
-    a = int(a)
-    c = a**.5
+    c = a ** .5
     return c
 
 class Calculator:
@@ -46,18 +36,18 @@ class Calculator:
         self.result = subtraction(a, b)
         return self.result
 
-    def multiplication(self, a, b):
+    def multiple(self, a, b):
         self.result = multiplication(a, b)
         return self.result
 
-    def division(self, a, b):
+    def div(self, a, b):
         self.result = division(a, b)
         return self.result
 
-    def square(self, a):
+    def squaring(self, a):
         self.result = square(a)
         return self.result
 
-    def square_root(self, a):
+    def square_rooting(self, a):
         self.result = square_root(a)
         return self.result
